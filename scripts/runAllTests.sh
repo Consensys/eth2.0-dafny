cbb6cf73cbdfbafb3f455a1a7682f6691ae527ce
TonyP4N <92439213+TonyP4N@users.noreply.github.com>
2024-02-25 20:20:57 +0000

    refactor: Delete irrelevant files and scripts

--- scripts/runAllTests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# use the docker container to run dafny
-
-# compile and run tests in the test dir
-docker run -v /Users/franck/development/eth2.0-dafny:/dafny -it dafny-2.3.0.10506 /bin/bash -c  'cd dafny' ; python3 scripts/runTests.py "test"
--- wiki/other-resources.md
+++
@@ -1,7 +0,0 @@
-[[up]](../README.md)
-
-# K-framework resources
-
-* [reference manual/documentation](https://github.com/kframework/k/blob/master/pending-documentation.md) WIP manual
-
-[[up]](../README.md)
